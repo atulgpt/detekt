1989a5388ecd91ee9f96c167b7cdcbc209e83a23
Atul Gupta <atlgpt17@gmail.com>
2023-12-02 19:27:47 +0530

    Add TC and indent the code for `Deprecation`

--- detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/Deprecation.kt
+++ detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/Deprecation.kt
@@ -27,8 +27,15 @@ class Deprecation(config: Config) : Rule(config) {
     override fun visitElement(element: PsiElement) {
         val diagnostic = hasDeprecationCompilerWarnings(element)
         if (diagnostic != null) {
-            val entity = if (element is KtNamedDeclaration) Entity.atName(element) else Entity.from(element)
-            report(CodeSmell(issue, entity, """${element.text} is deprecated with message "${diagnostic.b}""""))
+            val entity =
+                if (element is KtNamedDeclaration) Entity.atName(element) else Entity.from(element)
+            report(
+                CodeSmell(
+                    issue,
+                    entity,
+                    """${element.text} is deprecated with message "${diagnostic.b}""""
+                )
+            )
         }
         super.visitElement(element)
     }
--- detekt-rules-errorprone/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/DeprecationSpec.kt
+++ detekt-rules-errorprone/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/DeprecationSpec.kt
@@ -4,6 +4,8 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
 import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
+import org.jetbrains.kotlin.cli.common.setupLanguageVersionSettings
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.junit.jupiter.api.Test
 
@@ -47,5 +49,62 @@ class DeprecationSpec(private val env: KotlinCoreEnvironment) {
             }
         """.trimIndent()
         assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
+    }
+
+    @Test
+    fun `does not report when language settings is below than DeprecatedSinceKotlin version`() {
+        val code = """
+            @Deprecated("deprecation message")
+            @DeprecatedSinceKotlin(warningSince = "1.4")
+            fun foo() {
+                /* no-op */
+            }
+            
+            fun test() {
+                foo()
+            }
+        """.trimIndent()
+        val v = object : CommonCompilerArguments() {}
+        v.languageVersion = "1.3"
+        env.configuration.setupLanguageVersionSettings(v)
+        assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
+    }
+
+    @Test
+    fun `does report when language settings is above than DeprecatedSinceKotlin version`() {
+        val code = """
+            @Deprecated("deprecation message")
+            @DeprecatedSinceKotlin(warningSince = "1.4")
+            fun foo() {
+                /* no-op */
+            }
+            
+            fun test() {
+                foo()
+            }
+        """.trimIndent()
+        val compilerArgs = object : CommonCompilerArguments() {}
+        compilerArgs.languageVersion = "1.6"
+        env.configuration.setupLanguageVersionSettings(compilerArgs)
+        assertThat(subject.compileAndLintWithContext(env, code)).hasSize(1)
+    }
+
+    @Test
+    fun `does report when language settings is equal to DeprecatedSinceKotlin version`() {
+        val code = """
+            @Deprecated("deprecation message")
+            @DeprecatedSinceKotlin(warningSince = "1.4")
+            fun foo() {
+                /* no-op */
+            }
+            
+            fun test() {
+                foo()
+            }
+        """.trimIndent()
+        val compilerArgs = object : CommonCompilerArguments() {}
+        compilerArgs.languageVersion = "1.4"
+        env.configuration.setupLanguageVersionSettings(compilerArgs)
+        assertThat(subject.compileAndLintWithContext(env, code)).hasSize(1)
     }
 }
